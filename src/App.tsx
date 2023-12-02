8dd9877a80f9a4e5b14e7897d9dfc4d2b850a37a
Joseph Valle <josephrvalle@gmail.com>
2023-12-02 14:12:32 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { database } from './firebase-config';
+import { ref, get, child } from 'firebase/database';
 
-function App() {
+const App: React.FC = () => {
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const dbRef = ref(database, 'image_prompts'); // reference to the image_prompts node
+      try {
+        const snapshot = await get(dbRef);
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          setData(data);
+        } else {
+          console.log("No data available");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Render your data here */}
+        {data ? (
+          <div>{JSON.stringify(data)}</div>
+        ) : (
+          <div>No data loaded...</div>
+        )}
       </header>
     </div>
   );
-}
+
+};
 
 export default App;
