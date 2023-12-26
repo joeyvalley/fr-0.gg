1e9399b4c43c506c7bcd32ed3611d9ca8a92cb9f
Joseph Valle <josephrvalle@gmail.com>
2023-12-26 17:57:07 -0500

    fixed some CSS issues

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,7 @@ interface fr0gg {
 }
 
 const App: React.FC = () => {
-  const [numEntries, setNumEntries] = useState(100);
-  const appContainer = useRef<HTMLDivElement>(null);
   const [data, setData] = useState<fr0gg[]>([]);
-  const [paginatedData, setPaginatedData] = useState<fr0gg[]>([]);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [loading, setLoading] = useState(true);
 
   async function fetchData() {
@@ -31,7 +27,6 @@ const App: React.FC = () => {
           return dateB.getTime() - dateA.getTime();
         });
         setData(sortedData);
-        setPaginatedData(sortedData.slice(0, numEntries));
       } else {
         console.log("No data available");
       }
@@ -43,64 +38,33 @@ const App: React.FC = () => {
     }
   };
 
-  // Function to fetch data from firebase and store it in the data state.
   useEffect(() => {
-    if (windowWidth <= 400) {
-      setNumEntries(25);
-    } else if (windowWidth <= 600 && windowWidth > 400) {
-      setNumEntries(20);
-    } else if (windowWidth <= 800 && windowWidth > 600) {
-      setNumEntries(18);
-    }
-    else {
-      setNumEntries(24);
-    }
     fetchData();
   }, []);
 
-  // Function to copy prompt to clipboard
-  // function copyPrompt(prompt: string, date: string) {
-  //   navigator.clipboard.writeText(prompt);
-  //   setCopiedPrompts(prevState => ({ ...prevState, [date]: true }));
-  //   setTimeout(() => setCopiedPrompts(prevState => ({ ...prevState, [date]: false })), 1500);
-  // }
-
-
-  function ribbit() {
-    const sound = new Audio("/assets/ribbit.mp3");
-    try {
-      sound.play();
-      console.log('ribbit');
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-
   return (
-    <div className="app" ref={appContainer}>
+    <div className="app">
       <header>
         <a href="https://github.com/joeyvalley/fr0.gg">fr-0.gg</a>
       </header>
-      {data.length > 0 ? (
+      {loading ? (
+        <div className="loading">🐸</div>
+      ) : (
         <div className="fr0gg-container">
-          {paginatedData.map(entry => (
+          {data.map(entry => (
             <div className="fr0gg" key={entry.date}>
-              <img src={entry.image_url} alt={`fr0gg ${entry.date}`} onClick={() => { ribbit() }} />
-              {/* <div className='info'>
-                <span className="birthday">{entry.date}</span>
-              </div> */}
+              <img
+                src={entry.image_url}
+                alt={`fr0gg ${entry.date}`}
+              />
             </div>
           ))}
         </div>
-      ) : (
-        <div className="loading">🐸</div>
-      )
-      }
+      )}
       <div className='footer'>
         ok
       </div>
-    </div >
+    </div>
   );
 };
 
